62fcf5e78b57f8f40909915754efc80851802c5e
Lee J. Barrington <lee.j.barrington@gmail.com>
2015-11-21 21:50:22 -0500

    Passed tests for 1->I and 2->II and 3->III.

--- test/RomanNumeralsTest.java
+++ test/RomanNumeralsTest.java
@@ -14,4 +14,9 @@ public class RomanNumeralsTest {
         RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("II", romanNumerals.fromArabic(2));
     }
+    @Test
+    public void whenInputIs3OutputIsIII() {
+        RomanNumerals romanNumerals = new RomanNumerals();
+        assertEquals("III", romanNumerals.fromArabic(3));
+    }
 }
