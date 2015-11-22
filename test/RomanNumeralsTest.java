72730f7ef52defa65029e50bbb7ae7a9c39b9e19
Lee J. Barrington <lee.j.barrington@gmail.com>
2015-11-22 10:40:21 -0500

    Moved creation of RomanNumerals object from test methods to an @Before setUp method.

--- test/RomanNumeralsTest.java
+++ test/RomanNumeralsTest.java
@@ -1,4 +1,5 @@
 import com.pillarescent.RomanNumerals;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,30 +8,30 @@ import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertFalse;
 
 public class RomanNumeralsTest {
+    RomanNumerals romanNumerals; // To be initialized in each test!
+    @Before
+    public void setUp() {
+        romanNumerals = new RomanNumerals();
+    }
     @Test
     public void whenInputIs1OutputIsASingleI() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("I", romanNumerals.fromArabic(1));
     }
     @Test
     public void whenInputIs2OutputIsII() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("II", romanNumerals.fromArabic(2));
     }
     @Test
     public void whenInputIs3OutputIsIII() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         assertEquals("III", romanNumerals.fromArabic(3));
     }
     @Test
     public void whenInputIs4OutputDoesNotContainMoreThan3IsInARow() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         String romanFor4 = romanNumerals.fromArabic(4);
         assertFalse(romanFor4.contains("IIII"));
     }
     @Test
     public void whenInputIs157OutputIsCLVII() {
-        RomanNumerals romanNumerals = new RomanNumerals();
         String romanFor157 = romanNumerals.fromArabic(157);
         assertEquals("CLVII", romanFor157);
     }
