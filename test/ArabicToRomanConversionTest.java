bbead474c10faa3e3428c11bb8282d75f51b5884
Lee J. Barrington <lee.j.barrington@gmail.com>
2015-11-22 13:28:10 -0500

    Reworked ConversionTest class a bit for conciseness of @Test methods.

--- test/ArabicToRomanConversionTest.java
+++ test/ArabicToRomanConversionTest.java
@@ -1,97 +1,84 @@
 import com.pillarescent.ArabicToRomanConversion;
 
+import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 
 public class ArabicToRomanConversionTest  {
-    @Test
-    public void afterConstructingOn1QueryOnStateYieldsOrigArabicValAndEmptyRomanRepresentation() {
-        int currArabicVal;
-        String currRomanVal;
-        ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(1);
+    int currArabicVal;
+    String currRomanVal;
+
+    protected void setValuesFrom(ArabicToRomanConversion arabicToRomanConv) {
         currArabicVal = arabicToRomanConv.getCurrentArabicValue();
         currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+    }
+
+    @Test
+    public void afterConstructingOn1QueryOnStateYieldsOrigArabicValAndEmptyRomanRepresentation() {
+        ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(1);
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 1);
         assertEquals(currRomanVal, "");
     }
     @Test
     public void afterConstructingOnAnyVal_QueryOnStateYieldsOrigArabicValAndEmptyRomanRepresentation() {
-        int currArabicVal;
-        String currRomanVal;
         ArabicToRomanConversion arabicToRomanConv;
 
         arabicToRomanConv = new ArabicToRomanConversion(2);
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 2);
         assertEquals(currRomanVal, "");
 
         arabicToRomanConv = new ArabicToRomanConversion(5);
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 5);
         assertEquals(currRomanVal, "");
 
         arabicToRomanConv = new ArabicToRomanConversion(157);
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 157);
         assertEquals(currRomanVal, "");
     }
     @Test
     public void afterConstructingOn1AndShiftingRomanIsIAndArabicIs0() throws Exception {
-        int currArabicVal;
-        String currRomanVal;
         ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(1);
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 0);
         assertEquals(currRomanVal, "I");
     }
     @Test
     public void afterConstructingOn157AndShiftingRomanIsCAndArabicIs57() throws Exception {
-        int currArabicVal;
-        String currRomanVal;
         ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(157);
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 57);
         assertEquals(currRomanVal, "C");
     }
     @Test
     public void afterConstructingOn157AndTwoShiftsRomanIsCLAndArabicIs7() throws Exception {
-        int currArabicVal;
-        String currRomanVal;
         ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(157);
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 7);
         assertEquals(currRomanVal, "CL");
     }
     @Test
     public void afterConstructingOn157AndFiveShiftsRomanIsCLVIIAndArabicIs0() throws Exception {
-        int currArabicVal;
-        String currRomanVal;
         ArabicToRomanConversion arabicToRomanConv = new ArabicToRomanConversion(157);
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 7);
         assertEquals(currRomanVal, "CL");
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 1);
         assertEquals(currRomanVal, "CLVI");
         arabicToRomanConv.shiftNextNumeralValueFromArabicToRoman();
-        currArabicVal = arabicToRomanConv.getCurrentArabicValue();
-        currRomanVal = arabicToRomanConv.getCurrentRomanValue();
+        setValuesFrom(arabicToRomanConv);
         assertEquals(currArabicVal, 0);
         assertEquals(currRomanVal, "CLVII");
     }
