e055dabacf56e3190cec2267ac887c0cb50e5669
Lee J. Barrington <lee.j.barrington@gmail.com>
2015-11-22 19:17:09 -0500

    Added many test cases for "4s and 9s". All passing.

--- test/RomanNumeralsTest.java
+++ test/RomanNumeralsTest.java
@@ -1,4 +1,5 @@
 import com.pillarescent.RomanNumerals;
+import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -39,4 +40,31 @@ public class RomanNumeralsTest {
         String romanFor157 = romanNumerals.fromArabic(157);
         assertEquals("CLVII", romanFor157);
     }
+    @Test
+    public void whenInputIs9OutputIsIX() {
+        String romanFor9 = romanNumerals.fromArabic(9);
+        assertEquals("IX", romanFor9);
+    }
+    @Test
+    public void whenInputIs14OutputIsXIV() { assertEquals("XIV",  romanNumerals.fromArabic(14)); }
+    @Test
+    public void whenInputIs29OutputIsXXIX() { assertEquals("XXIX", romanNumerals.fromArabic(29)); }
+    @Test
+    public void whenInputIs48OutputIsXLVIII() { assertEquals("XLVIII", romanNumerals.fromArabic(48)); }
+    @Test
+    public void whenInputIs84OutputIsLXXXIV() { assertEquals("LXXXIV", romanNumerals.fromArabic(84)); }
+    @Test
+    public void whenInputIs90OutputIsXC() { assertEquals("XC", romanNumerals.fromArabic(90)); }
+    @Test
+    public void whenInputIs149OutputIsCXLIX() { assertEquals("CXLIX", romanNumerals.fromArabic(149)); }
+    @Test
+    public void whenInputIs399OutputIsCCCXCIX() { assertEquals("CCCXCIX", romanNumerals.fromArabic(399)); }
+    @Test
+    public void whenInputIs888OutputIsDCCCLXXX() { assertEquals("DCCCLXXXVIII", romanNumerals.fromArabic(888)); }
+    @Test
+    public void whenInputIs3444OutputIsMMMCDXLIV() { assertEquals("MMMCDXLIV", romanNumerals.fromArabic(3444)); }
+    @Test
+    public void whenInputIs3888OutputIsMMMDCCCLXXXVIII() { assertEquals("MMMDCCCLXXXVIII", romanNumerals.fromArabic(3888)); }
+    @Test
+    public void whenInputIs3999OutputIsMMMCMXCIX() { assertEquals("MMMCMXCIX", romanNumerals.fromArabic(3999)); }
 }
