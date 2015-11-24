0d3ab9690279dd99e73e0d67aa22a3c42d7379e2
Lee J. Barrington <lee.j.barrington@gmail.com>
2015-11-24 09:52:24 -0500

    Added protected utensil methods for common test activity to DRY up ArabicRomanIOTest.

--- test/ArabicRomanIOTest.java
+++ test/ArabicRomanIOTest.java
@@ -1,6 +1,5 @@
 import com.pillarescent.ArabicRomanIO;
 
-import junit.framework.TestCase;
 import org.junit.Test;
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
@@ -9,44 +8,55 @@ import java.io.StringWriter;
 
 
 public class ArabicRomanIOTest {
+
+    protected void takeValAsInputAndLookForOutputPrefix(String input, String expectedPrefix) {
+        StringWriter outputSink = new StringWriter();
+        ArabicRomanIO io = new ArabicRomanIO(input, outputSink);
+        io.handleInputs();
+        String actualOutput = outputSink.toString().replaceAll("\\s+$", "");
+        assertTrue(actualOutput.startsWith(expectedPrefix));
+    }
+    protected void takeValAsInputAndLookForSpecificOutput(String input, String expectedOutput) {
+        StringWriter outputSink = new StringWriter();
+        ArabicRomanIO io = new ArabicRomanIO(input, outputSink);
+        io.handleInputs();
+        String actualOutput = outputSink.toString().replaceAll("\\s+$", "");
+        assertEquals(expectedOutput, actualOutput);
+    }
+
     @Test
-    public void doInputGetSomeKindOfOutput() {
+    public void doInputGetAnyKindOfOutput() {
         StringWriter outputSink = new StringWriter();
         ArabicRomanIO io = new ArabicRomanIO("1", outputSink);
         io.handleInputs();
         String output = outputSink.toString();
         junit.framework.TestCase.assertFalse(output.length() == 0);
     }
+
     @Test
     public void take1asInputAndOutputI() {
-        StringWriter outputSink = new StringWriter();
-        ArabicRomanIO io = new ArabicRomanIO("1", outputSink);
-        io.handleInputs();
-        String output = outputSink.toString().replaceAll("\\s+$", "");
-        assertEquals("I", output);
+         takeValAsInputAndLookForSpecificOutput("1", "I");
     }
     @Test
     public void take3999asInputAndOutputMMMCMXCIX() {
-        StringWriter outputSink = new StringWriter();
-        ArabicRomanIO io = new ArabicRomanIO("3999", outputSink);
-        io.handleInputs();
-        String output = outputSink.toString().replaceAll("\\s+$", "");
-        assertEquals("MMMCMXCIX", output);
+        takeValAsInputAndLookForSpecificOutput("3999", "MMMCMXCIX");
     }
     @Test
     public void takeNoInputAndOutput_NOTHING_() {
-        StringWriter outputSink = new StringWriter();
-        ArabicRomanIO io = new ArabicRomanIO("", outputSink);
-        io.handleInputs();
-        String output = outputSink.toString().replaceAll("\\s+$", "");
-        assertEquals("", output);
+        takeValAsInputAndLookForSpecificOutput("", "");
     }
     @Test
     public void takeNonNumericInputAndOutput_ErrorMessage_() {
-        StringWriter outputSink = new StringWriter();
-        ArabicRomanIO io = new ArabicRomanIO("zzz", outputSink);
-        io.handleInputs();
-        String output = outputSink.toString().replaceAll("\\s+$", "");
-        assertTrue(output.startsWith("Error: expected a regular number "));
+        takeValAsInputAndLookForOutputPrefix("zzz", "Error: expected a regular number ");
+    }
+    @Test
+    public void takeInputThatIsTooBigAndOutput_ErrorMessage_() {
+        String bigNumString = "4000";
+        takeValAsInputAndLookForOutputPrefix(bigNumString, "Error: Number " + bigNumString + " is too large ");
+    }
+    @Test
+    public void takeInputThatIsTooSmallAndOutput_ErrorMessage_() {
+        String smallNumString = "-1";
+        takeValAsInputAndLookForOutputPrefix(smallNumString, "Error: Number " + smallNumString + " is too small ");
     }
 }
